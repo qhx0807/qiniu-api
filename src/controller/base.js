e20ef19f4da6bd36a415376f43983b0e6a316054
qhx0807 <820240134@qq.com>
2018-12-12 22:56:00 +0800

    cors headers

--- src/controller/base.js
+++ src/controller/base.js
@@ -1,8 +1,8 @@
 module.exports = class extends think.Controller {
   __before () {
     this.header('Access-Control-Allow-Origin', '*');
-    this.header('Access-Control-Allow-Headers', 'x-requested-with');
-    this.header('Access-Control-Request-Method', 'GET,POST,PUT,DELETE');
+    this.header('Access-Control-Allow-Headers', 'authorization,content-type,x-requested-with,Origin,Accept');
+    this.header('Access-Control-Request-Method', 'PUT,POST,GET,DELETE,OPTIONS');
     this.header('Access-Control-Allow-Credentials', 'true');
   }
 };
